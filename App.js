85ab0df561db1c2eb6e1ae830775fe468a64a5ab
Lucas Pereira <lp@quint.com.br>
2023-03-19 20:23:03 -0300

    iniciando o projeto

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Title } from './src/components/Title';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Title />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
